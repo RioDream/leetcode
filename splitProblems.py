9841b6920123f3c41cbe3c1be23703b7a34fa569
李傲 <byr.li.ao@gmail.com>
2014-07-16 08:02:24 +0800

    modify the script and it works better now

--- splitProblems.py
+++ splitProblems.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python
 
+
+import os
+path = "./"
+title = "problems"
+new_path = os.path.join(path, title)
+if not os.path.isdir(new_path):
+    os.makedirs(new_path)
+
 summaryFile = "summary100.md"
 
-with open(summaryFile,"r") as f:
+
+f = None
+
+with open(summaryFile,"r") as fs:
 	while(True):
-		line = f.readline()
+		line = fs.readline()
 		if not line:
-			file.close()
+			f.close()
 			break
 		if line.startswith("##") and line[2]!="#":
-			file.close()
+			if f!=None:
+				f.close()
 			problemName = line[2:]
-			file = open(problemName+".md","wa")
-		print file
-		file.write("df")
+			print problemName
+			f = open(new_path+"/"+problemName+".md","wa")
+		if f:
+			f.write(line)
 			
 		
